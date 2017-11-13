c320ddd0f4538437d037b70a6f8d5025082c4291
MimiMagusin <mimi_magusin@hotmail.com>
2017-11-13 16:55:16 +0100

    Divide student over different arrays based on their last color

--- algorithm.rb
+++ algorithm.rb
@@ -5,39 +5,43 @@ s4 = { name: "Thimothy", evaluations: [{ comment: "yey", color: "green" }, { com
 s5 = { name: "Joop", evaluations: [{ comment: "yey", color: "red" }, { comment: "yey", color: "yellow" }, { comment: "yey", color: "red" }]}
 s6 = { name: "Jannie", evaluations: [{ comment: "yey", color: "red" }, { comment: "yey", color: "yellow" }, { comment: "yey", color: "green" }]}
 s7 = { name: "Eva", evaluations: [{ comment: "yey", color: "yellow" }]}
+s8 = { name: "Tobias", evaluations: []}
 
-students = [s1, s2, s3, s4, s5, s6, s7,]
+students = [s1, s2, s3, s4, s5, s6, s7, s8]
+
+    red = []
+    yellow = []
+    green = []
+
+    students.each do |student|
+      if student[:evaluations].length < 1
+        red.push(student)
+      elsif student[:evaluations].last[:color] == "green"
+        green.push(student)
+      elsif student[:evaluations].last[:color] == "yellow"
+        yellow.push(student)
+      else
+        red.push(student)
+      end
+    end
+    
+    #tests
+    puts s1
+    puts s1[:name]
+    puts"********************"
+    puts s1[:evaluations]
+    puts "********************"
+    puts s1[:evaluations].last[:color]
+    puts "********************"
+
+    puts "********************"
+    puts "red: #{red}"
+    puts "********************"
+    puts "yellow: #{yellow}"
+    puts "********************"
+    puts "green: #{green}"
 
 
-
- puts s1
- puts s1[:name]
- puts"********************"
- puts s1[:evaluations]
- puts "********************"
- puts s1[:evaluations].last[:color]
- puts "********************"
- puts "********************"
- students.each { |student| puts student[:evaluations].last[:color] }
-
-
-#   def student_colors
-#     red = []
-#     yellow = []
-#     green = []
-#
-#     Student.all.each |student|
-#       if student.evaluations.last.color = "red"
-#         red.push(student)
-#       elsif student.evaluations.last.color = "yellow"
-#         yellow.push(student)
-#       else
-#         green.push(student)
-#       end
-#     end
-#
-#     return pick_a_color(red, yellow, green)
-#   end
 #
 #   def pick_a_color(red, yellow, green)
 #     random_number = rand(100)
