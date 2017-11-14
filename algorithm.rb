81e83eb44cfd8db3484c67fb745596fca1b1061f
MimiMagusin <mimi_magusin@hotmail.com>
2017-11-14 11:26:38 +0100

    Test algorithm

--- algorithm.rb
+++ algorithm.rb
@@ -8,10 +8,14 @@ s7 = { name: "Eva", evaluations: [{ comment: "yey", color: "yellow" }]}
 s8 = { name: "Tobias", evaluations: []}
 
 students = [s1, s2, s3, s4, s5, s6, s7, s8]
-red = []
-yellow = []
-green = []
-random_number = rand(100)
+
+
+
+  def colored_students(students)
+
+    red = []
+    yellow = []
+    green = []
 
     students.each do |student|
       if student[:evaluations].length < 1
@@ -24,32 +28,75 @@ random_number = rand(100)
         red.push(student)
       end
     end
+    puts "red_array: #{red}"
+    puts "green_array: #{green}"
+    puts "yellow_array: #{yellow}"
+    puts "********************"
+    pick_a_student(red, green, yellow)
+  end
 
-    if random_number < 51
-      random_student = red[rand(red.length - 1)]
-    elsif random_number < 84
-      random_student = yellow[rand(yellow.length - 1)]
+  def pick_a_student(red, green, yellow)
+    random_number = rand(100)
+    if random_number < 51 && red.length > 0
+      @random_student = red[rand(red.length - 1)]
+    elsif random_number < 84 && yellow.length > 0
+      @random_student = yellow[rand(yellow.length - 1)]
+    elsif random_number > 83 && green.length > 0
+      @random_student = green[rand(green.length - 1)]
     else
-      random_student = green[rand(green.length - 1)]
+      random_number = rand(100)
     end
-
+    puts "random_student: #{@random_student}"
+    puts "**********"
+  end
 
     #tests
     puts s1
     puts s1[:name]
-    puts"********************"
     puts s1[:evaluations]
-    puts "********************"
     puts s1[:evaluations].last[:color]
     puts "********************"
+    colored_students(students)
 
-    puts "********************"
-    puts "red: #{red}"
-    puts "********************"
-    puts "yellow: #{yellow}"
-    puts "********************"
-    puts "green: #{green}"
-    puts "********************"
-    puts "********************"
-    puts "********************"
-    puts "random_student: #{random_student}"
+    def hundred_students_asked(students)
+      all_students_asked = []
+      100.times do
+        colored_students(students)
+        all_students_asked.push(@random_student)
+      end
+      puts "********************"
+      puts "all_students_asked: #{all_students_asked}"
+      students_by_color(all_students_asked)
+    end
+
+    def students_by_color(all_students_asked)
+      test_red = []
+      test_yellow = []
+      test_green = []
+
+      all_students_asked.each do |student|
+        if student[:evaluations].length < 1
+          test_red.push(student)
+        elsif student[:evaluations].last[:color] == "green"
+          test_green.push(student)
+        elsif student[:evaluations].last[:color] == "yellow"
+          test_yellow.push(student)
+        else
+          test_red.push(student)
+        end
+      end
+      percentage(test_red, test_yellow, test_green)
+    end
+
+    def percentage(test_red, test_yellow, test_green)
+      red_percentage = test_red.length
+      yellow_percentage = test_yellow.length
+      green_percentage = test_green.length
+       puts"****************"
+       puts "red: #{red_percentage}%"
+       puts "yellow: #{yellow_percentage}%"
+       puts "green: #{green_percentage}%"
+    end
+
+
+    hundred_students_asked(students)
--- app/models/random_student.rb
+++ app/models/random_student.rb
@@ -1,29 +1,27 @@
 class RandomStudent < ActiveRecord::Base
   def student_colors
-    red_students = []
-    yellow_students = []
-    green_students = []
+    red = []
+    yellow = []
+    green = []
 
     Student.all.each |student|
       if student.evaluations.last.color = "red"
-        red_students.push(student)
+        red.push(student)
       elsif student.evaluations.last.color = "yellow"
-        yellow_students.push(student)
+        yellow.push(student)
       else
-        green_students.push(student)
+        green.push(student)
       end
     end
 
-    return pick_a_color
+    return pick_a_color(red, yellow, green)
   end
 
-  def pick_a_color
+  def pick_a_color(red, yellow, green)
     random_number = rand(100)
 
     if random_number < 51
-      Student.all.each |student|
-        selected_students.push(student.evaluations.last.color.find_by(color: "red")
-      end
+      red.
     elsif random_number < 84
       Student.all.each |student|
         selected_students = student.evaluations.last.color.find_by(color: "yellow")
